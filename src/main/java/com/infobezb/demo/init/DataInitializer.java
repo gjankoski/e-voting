37acd496ead808068168de23f0bb4353188a0739
vladimir-jovchev <vladimir.jovchev@students.finki.ukim.mk>
2021-09-22 15:19:11 +0200

    add email sender

--- src/main/java/com/infobezb/demo/init/DataInitializer.java
+++ src/main/java/com/infobezb/demo/init/DataInitializer.java
@@ -8,16 +8,35 @@ import com.infobezb.demo.repository.CandidateRepository;
 import com.infobezb.demo.repository.UserRepository;
 import com.infobezb.demo.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+
+
 @Component
 public class DataInitializer {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     UserService userService;
     CandidateRepository candidateRepository;
@@ -28,7 +47,7 @@ public class DataInitializer {
     }
 
     @PostConstruct
-    void init() {
+    void init() throws MessagingException, IOException {
         this.userService.register("testuser", "testpassword", "testemail@gmail.com");
         this.userService.register("Vladimir", "1234pass", "t.budoski@hotmail.com");
         this.userService.register("Gjoko", "1234pass", "t.budoski@hotmail.com");
@@ -46,6 +65,30 @@ public class DataInitializer {
             System.out.println("hadbjadn asd");
         }*/
 
-    }
+        LocalDateTime date1 = LocalDateTime.now();
+        LocalDateTime electionDate = LocalDateTime.of(2021, 9, 22, 10, 15, 45); //Da napisime soodvete datum XD
 
+        if(date1.isAfter(electionDate)) {
+            sendEmailWithAttachment();
+        }
+
+}
+
+    void sendEmailWithAttachment() throws MessagingException, IOException {
+
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
+        //helper.setTo("to_@email");
+
+        //helper.setText("<h1>Check attachment for image!</h1>", true);
+
+        //FileSystemResource file = new FileSystemResource(new File("path/android.png"));
+        helper.addAttachment("Вашиот сертификат !", new ClassPathResource("cert")); // кај cert да се написит кој серификат се пуштат
+
+        javaMailSender.send(msg);
+
+    }
 }
